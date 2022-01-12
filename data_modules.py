a1c2cb5a82c4ff2d415a8051bef4f8359c3e35bc
johnny <wasilewskij823@gmail.com>
2022-01-12 09:20:20 +0100

    fix

--- data_modules.py
+++ data_modules.py
@@ -90,13 +90,11 @@ class MNISTDataModule(pl.LightningDataModule):
 
     def setup(self, stage=None):
         if stage == 'fit' or stage is None:
-            mnist_full = MNIST(self.data_dir, train=True, transform=self.transform,
-                               target_transform=lambda x: float(x<5))
+            mnist_full = MNIST(self.data_dir, train=True, transform=self.transform)# target_transform=lambda x: float(x<5))
             self.mnist_train, self.mnist_val = random_split(mnist_full, [55000, 5000])
 
         if stage == 'test' or stage is None:
-            self.mnist_test = MNIST(self.data_dir, train=False, transform=self.transform,
-                                   target_transform=lambda x: float(x<5))
+            self.mnist_test = MNIST(self.data_dir, train=False, transform=self.transform) #,                                   target_transform=lambda x: float(x<5))
 
     def train_dataloader(self):
         return DataLoader(self.mnist_train, batch_size=self.batch_size)
